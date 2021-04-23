70a96056abc95ef400996f6b7780056972a03d47
Gabriel02ps <61991172+Gabriel02ps@users.noreply.github.com>
2021-04-23 01:23:07 -0300

    Roteamento e página episódio

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,12 +1,14 @@
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
-import { GetStaticPaths, GetStaticProps } from "next";
-import { api } from "../../services/api";
-import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { GetStaticPaths, GetStaticProps } from "next";
 
-import styles from './episode.module.scss'
+import { api } from "../../services/api";
+import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
+
+import styles from "./episode.module.scss";
 
 type Episode = {
   id: string;
@@ -25,6 +27,12 @@ type EpisodeProps = {
 };
 
 export default function Episode({ episode }: EpisodeProps) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <p>carregando</p>;
+  }
+
   return (
     <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
@@ -61,8 +69,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 2,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const paths = data.map((episode) => {
+    return {
+      params: {
+        slug: episode.id,
+      },
+    };
+  });
+
   return {
-    paths: [],
+    paths,
     fallback: "blocking",
   };
 };
